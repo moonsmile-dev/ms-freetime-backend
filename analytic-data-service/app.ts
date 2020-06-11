b3fa53033d916fec6426b7972dc8caf7b89b5dce
quangnd <quang.ngo@youthdev.net>
2020-06-11 23:19:01 +0700

    Restructure modules

---
+++ analytic-data-service/app.ts
@@ -0,0 +1,25 @@
+import { Application, Router, Status } from "https://deno.land/x/oak/mod.ts";
+
+import timer from "./middleware/timer.ts";
+import logger from "./middleware/logger.ts";
+
+const app = new Application();
+const router = new Router();
+
+app.use(logger);
+app.use(timer);
+
+router.get("/", (ctx) => {
+  ctx.response.status = Status.OK;
+  ctx.response.type = "json";
+  ctx.response.body = {
+    status: "success",
+    message: "Hello World!",
+    data: null,
+  };
+});
+
+app.use(router.routes());
+
+console.log("app running -> http://localhost:3000");
+await app.listen({ port: 3000 });
