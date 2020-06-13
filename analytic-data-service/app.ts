56ac0a29e5da1c45dc3b747b52795a19ab27f648
quangnd <quang.ngo@youthdev.net>
2020-06-13 16:32:56 +0700

    remove unused lib in app.ts

--- analytic-data-service/app.ts
+++ analytic-data-service/app.ts
@@ -4,10 +4,7 @@ import timer from "./middleware/timer.ts";
 import logger from "./middleware/logger.ts";
 import error from "./middleware/error.ts";
 import { success } from "./common/responses.ts";
-import config from "./nessie.config.ts";
 import { initOrm } from "./modules/core/orm.config.ts";
-import { UserModel } from "./modules/users/model.ts";
-import { dso, Where } from "https://deno.land/x/dso@v1.0.0/mod.ts";
 
 const app = new Application();
 const router = new Router();
