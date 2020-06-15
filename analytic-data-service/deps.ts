5967f8b90e3b3922801539a6669218fb1df63823
quangnd <quang.ngo@youthdev.net>
2020-06-15 21:48:24 +0700

    define depencency for profile

---
+++ analytic-data-service/deps.ts
@@ -0,0 +1,26 @@
+export {
+  Model,
+  BaseModel,
+  Field,
+  FieldType,
+  dso,
+  Where,
+} from "https://deno.land/x/dso@v1.0.0/mod.ts";
+export { ensureDir } from "https://deno.land/std/fs/mod.ts";
+export {
+  hourly,
+  everyMinute,
+} from "https://deno.land/x/deno_cron@v1.0.0/cron.ts";
+
+// export * from "https://deno.land/x/nessie@v1.0.0/cli.ts";
+export {
+  ClientMySQL,
+  Migration,
+} from "https://deno.land/x/nessie@v1.0.0/mod.ts";
+export { Schema } from "https://deno.land/x/nessie@v1.0.0/qb.ts";
+export {
+  Application,
+  Router,
+  Status,
+  isHttpError,
+} from "https://deno.land/x/oak@v5.2.0/mod.ts";
