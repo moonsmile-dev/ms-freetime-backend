b651086fcbd1f078f53b1f7142505a91bc01b1e7
quangnd <quang.ngo@youthdev.net>
2020-06-20 15:43:01 +0700

    Build cms in docker cluster.

--- ms-cms/src/App.tsx
+++ ms-cms/src/App.tsx
@@ -2,8 +2,10 @@ import * as React from "react";
 import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 import { PhotoList } from "./Tinders/PhotoResource";
+import { ANALYTIC_BACKEND_API } from "./common/contants";
 
-const dataProvider = jsonServerProvider("http://127.0.0.1:3000");
+console.log(`BACKEND API: ${ANALYTIC_BACKEND_API}`);
+const dataProvider = jsonServerProvider(ANALYTIC_BACKEND_API);
 const App = () => (
   <Admin dataProvider={dataProvider}>
     <Resource name="photos" list={PhotoList} />
--- ms-cms/src/common/contants.ts
+++ ms-cms/src/common/contants.ts
@@ -1,4 +1,4 @@
-export const ANALYTIC_BACKEND_API = "http://127.0.0.1:3000";
+export const ANALYTIC_BACKEND_API = process.env.REACT_APP_ANALYTIC_BACKEND_API;
 export const REACT_PHOTO_API = `${ANALYTIC_BACKEND_API}/photos/{0}/react`;
 
 export const PHOTO_STATUS_LIKE = 1;
